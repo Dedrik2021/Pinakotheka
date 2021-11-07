03c097679aaa2f0537b3783529d09c2c2719e1f6
Dedrik2021 <81495947+Dedrik2021@users.noreply.github.com>
2021-11-08 00:15:19 +0100

    style

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,37 @@
 $(function() {
     
-    // $(document).mouseup(function (e) {
-    //     var container = $('.menu, .modal');
-    //     if (container.has(e.target).length === 0 && $('body').hasClass('lock')) {
-    //         $('body').removeClass('.lock');
-    //     }
-    // });
+    $(document).mouseup(function (e) {
+        var container = $('.menu, .modal');
+        if (container.has(e.target).length === 0 && $('body').hasClass('lock')) {
+            $('body').removeClass('lock');
+        }
+    });
 
     $(document).mouseup(function (e) {
         var container = $('.menu');
         if (container.has(e.target).length === 0 && $('.menu__form, .menu__search, .overlay, .menu__btn').hasClass('active')) {
             $('.menu__form, .menu__search, .menu__btn').removeClass('active');
         }
+    });
+
+    $(document).mouseup(function (e) {
+        var container = $('.modal, .menu');
+        if (container.has(e.target).length === 0 && $('.overlay, .modal, .menu__inner, .menu__burger, .menu__box').hasClass('active')) {
+            $('.overlay, .modal, .menu__inner, .menu__burger, .menu__box').removeClass('active');
+        }
+    });
+
+    $(document).mouseup(function (e) {
+        var container = $('.menu');
+        if (container.has(e.target).length === 0 && $('.overlay').hasClass('_active')) {
+            $('.overlay').removeClass('_active');
+        }
+    });
+
+    $( ".menu__item--dropdown" ).click(function() {
+        $( ".menu-dropdown" ).slideToggle( "slow", function() {
+        
+        });
     });
 
     $('.hero__pick-slider').slick({
@@ -28,8 +48,8 @@ $(function() {
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                    centerPadding: '50px',
-
+                    fade: true,
+                    centerMode: false,
                 }
             },
             {
@@ -37,10 +57,9 @@ $(function() {
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                    centerPadding: '0',
-
+                    fade: true,
                 }
-            }
+            },
         ]
     });
 
@@ -56,6 +75,36 @@ $(function() {
         autoplay: true,
     });
 
+    
+    let isMobile = false;
+    let isDestroyed = true;
+    if (window.innerWidth <= 576) isMobile = true;
+
+    function initializeSlick() {
+        $('.gallery-filters').slick({
+            prevArrow: '<button type="button" class="slick-btn slick-btn--prev"><<<</button>',
+            nextArrow: '<button type="button" class="slick-btn slick-btn--next">>>></button>',
+            slidesToScroll: 1,
+        });
+        isDestroyed = false;
+    }
+
+    function destroySlick() {
+        $('.gallery-filters').slick('unslick');
+            isDestroyed = true;
+    }
+
+    if (isMobile && isDestroyed) initializeSlick();
+    else if (!isMobile && !isDestroyed) destroySlick();
+
+    window.addEventListener('resize', function () {
+        if (!isMobile && window.innerWidth <= 576) isMobile = true;
+        if (isMobile && window.innerWidth > 576) isMobile = false;
+
+        if (isMobile && isDestroyed) initializeSlick();
+        else if (!isMobile && !isDestroyed) destroySlick();
+    });
+
     $(".painting-card__stars").rateYo({
         rating: 3.6,
         starWidth: '14px',
@@ -65,6 +114,76 @@ $(function() {
         rating: "80%",
         readOnly: true,
     });
+
+    $('.btn--item').on('click', function() {
+        $('.btn--item').toggleClass('active')
+    })
+
+    $('.btn--style').on('click', function() {
+        $('.btn--style').toggleClass('active')
+    })
+
+    $('.btn--material').on('click', function() {
+        $('.btn--material').toggleClass('active')
+    })
+
+    $('.btn--color').on('click', function() {
+        $('.btn--color').toggleClass('active')
+    })
+
+    $('.btn--price').on('click', function() {
+        $('.btn--price').toggleClass('active')
+    })
+
+    $('.btn--size').on('click', function() {
+        $('.btn--size').toggleClass('active')
+    })
+
+
+
+
+    $( ".btn--style" ).click(function() {
+        $( ".creations__filters-box--style" ).slideToggle( "slow", function() {
+        
+        });
+    });
+
+    $( ".btn--size" ).click(function() {
+        $( ".creations__filters-box--size" ).slideToggle( "slow", function() {
+        
+        });
+    });
+
+    $( ".btn--price" ).click(function() {
+        $( ".creations__filters-box--price" ).slideToggle( "slow", function() {
+        
+        });
+    });
+
+    $( ".btn--item" ).click(function() {
+        $( ".creations__filters-box--item" ).slideToggle( "slow", function() {
+        
+        });
+    });
+
+    $( ".btn--material" ).click(function() {
+        $( ".creations__filters-box--material" ).slideToggle( "slow", function() {
+        
+        });
+    });
+
+    $( ".btn--color" ).click(function() {
+        $( ".creations__filters-box--color" ).slideToggle( "slow", function() {
+        
+        });
+    });
+
+    const cards = document.querySelectorAll('.creations .creations__wrapper');
+    cards.forEach((card)=>{
+            card.querySelector('.creations__label').addEventListener('click', () => {
+            card.querySelector('.creations__label').classList.toggle('active');
+        });
+    })
 
     mixer = mixitup('.gallery__content');
 });
@@ -78,12 +197,16 @@ let header = document.querySelector('.header');
     menuBtn = document.querySelector('.menu__btn');
     menuForm = document.querySelector('.menu__form');
     menuSearch = document.querySelector('.menu__search');
+    menuBurger = document.querySelector('.menu__burger');
+    menuBlock = document.querySelector('.menu__inner');
+    menuBox = document.querySelector('.menu__box');
     
 window.addEventListener('scroll', checkScroll);
 document.addEventListener('DOMContentLoaded', checkScroll);
 enterBtn.addEventListener('click', onEnterBtn);
 btnClose.addEventListener('click', onBtnClose);
 menuBtn.addEventListener('click', onMenuBtn);
+menuBurger.addEventListener('click', onMenuBurger);
 
 function checkScroll() {
     let scrollPos = window.scrollY;
@@ -96,9 +219,12 @@ function checkScroll() {
 }
 
 function onEnterBtn() {
-    overlay.classList.toggle('active')
-    modalEl.classList.toggle('active')
-    bodyLock.classList.toggle('lock')
+    overlay.classList.add('active')
+    modalEl.classList.add('active')
+    menuBlock.classList.remove('active')
+    menuBox.classList.remove('active')
+    menuBurger.classList.remove('active')
+    bodyLock.classList.add('lock')
 };
 
 function onBtnClose() {
@@ -113,6 +239,22 @@ function onMenuBtn() {
     menuSearch.classList.toggle('active')
 };
 
+function onMenuBurger() {
+    menuBlock.classList.toggle('active')
+    menuBurger.classList.toggle('active')
+    overlay.classList.toggle('_active')
+    overlay.classList.remove('active')
+    menuBox.classList.toggle('active')
+    modalEl.classList.remove('active')
+    bodyLock.classList.add('lock')
+};
+
+menuBurger.addEventListener('click', function(){
+    let menuBurger = document.querySelector('.menu__burger')
+    menuBurger.addEventListener('click', function(){
+        bodyLock.classList.toggle('lock')
+    })
+})
 
 let tab = function () {
     let tabNav = document.querySelectorAll('.btns-tabs__btn'),
@@ -141,3 +283,10 @@ let tab = function () {
 };
 
 tab();
+
+window.increment = function(event) {
+    var btn = event.target;
+    btn.clicks = ((btn.clicks || 0) + 1) % 2;
+    window.clicks = (window.clicks || 0) + btn.clicks * 2 - 1;
+    document.getElementById("clicks").innerText = window.clicks;
+}
--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ function scripts() {
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/slidetoggle/dist/slidetoggle.js',
         'node_modules/mixitup/dist/mixitup.js',
         'app/js/main.js',
     ])
