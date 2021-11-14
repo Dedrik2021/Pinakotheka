ba317126cc7d6b591d40672f18ad3094749db63b
Dedrik2021 <81495947+Dedrik2021@users.noreply.github.com>
2021-11-14 20:42:29 +0100

    added pages creations, creations-details

--- app/js/main.js
+++ app/js/main.js
@@ -15,9 +15,9 @@ $(function() {
     });
 
     $(document).mouseup(function (e) {
-        var container = $('.modal, .menu');
-        if (container.has(e.target).length === 0 && $('.overlay, .modal, .menu__inner, .menu__burger, .menu__box').hasClass('active')) {
-            $('.overlay, .modal, .menu__inner, .menu__burger, .menu__box').removeClass('active');
+        var container = $('.modal, .menu, .creations');
+        if (container.has(e.target).length === 0 && $('.overlay, .modal, .menu__inner, .menu__burger, .menu__box, .creations__filters').hasClass('active')) {
+            $('.overlay, .modal, .menu__inner, .menu__burger, .menu__box, .creations__filters').removeClass('active');
         }
     });
 
@@ -105,12 +105,20 @@ $(function() {
         else if (!isMobile && !isDestroyed) destroySlick();
     });
 
-    $(".painting-card__stars").rateYo({
-        rating: 3.6,
+    $(".painting-card__stars, .author-card__stars").rateYo({
         starWidth: '14px',
         normalFill: '#d6d6d6',
         ratedFill: '#ffcc00',
         spacing   : "1px",
+        rating: "80%",
+        readOnly: true,
+    });
+
+    $(".painting-card__card-stars").rateYo({
+        starWidth: '20px',
+        normalFill: '#d6d6d6',
+        ratedFill: '#ffcc00',
+        spacing   : "5px",
         rating: "80%",
         readOnly: true,
     });
@@ -138,9 +146,6 @@ $(function() {
     $('.btn--size').on('click', function() {
         $('.btn--size').toggleClass('active')
     })
-
-
-
 
     $( ".btn--style" ).click(function() {
         $( ".creations__filters-box--style" ).slideToggle( "slow", function() {
@@ -185,80 +190,102 @@ $(function() {
         });
     })
 
+    $('.filters-items').styler();
+
+    $('.filters-view__btn').on('click', function() {
+        $('.filters-view__btn').removeClass('active');
+        $(this).addClass('active')
+    });
+
+    $('.btn-list').on('click', function() {
+        $('.modern__item, .modern__list, .modern').addClass('list');
+    });
+
+    $('.btn-grid').on('click', function() {
+        $('.modern__item, .modern__list, .modern').removeClass('list');
+    });
+
+    $('.details-card__thumb-slider').slick({
+        asNavFor: '.details-card__big-slider',
+        focusOnSelect: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        vertical: true,
+        draggable: false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 577,
+                settings: {
+                    vertical: false,
+                }
+            },
+        ]
+    });
+    
+    $('.details-card__big-slider').slick({
+        asNavFor: '.details-card__thumb-slider',
+        draggable: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 577,
+                settings: {
+                    draggable: true,
+                    slidesToShow: 1,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                }
+            },
+        ]
+    });
+
+    $('.tabs-btns__btn').on('click', function() {
+        $('.tabs-btns__btn').removeClass('active');
+        $(this).addClass('active')
+    });
+
+    $('.btns-tabs__btn').on('click', function() {
+        $('.btns-tabs__btn').removeClass('active');
+        $(this).addClass('active')
+    });
+
+    let tab = function () {
+        let tabNav = document.querySelectorAll('.btns-tabs__btn'),
+            tabContent = document.querySelectorAll('.modal-form__item'),
+            tabName;
+    
+        tabNav.forEach(item => {
+            item.addEventListener('click', selectTabNav)
+        });
+    
+        function selectTabNav() {
+            tabNav.forEach(item => {
+                item.classList.remove('active');
+            });
+            this.classList.add('active');
+            tabName = this.getAttribute('data-content');
+            selectTabContent(tabName);
+        }
+    
+        function selectTabContent(tabName) {
+            tabContent.forEach(item => {
+                item.classList.contains(tabName) ? item.classList.add('active') : item.classList.remove('active');
+            })
+        }
+    };
+    
+    tab();
+
     mixer = mixitup('.gallery__content');
 });
 
-let header = document.querySelector('.header');
-    enterBtn = document.querySelector('.menu__enter-btn');
-    modalEl = document.querySelector('.modal');
-    overlay = document.querySelector('.overlay');
-    btnClose = document.querySelector('.btn--close');
-    bodyLock = document.getElementById('body');
-    menuBtn = document.querySelector('.menu__btn');
-    menuForm = document.querySelector('.menu__form');
-    menuSearch = document.querySelector('.menu__search');
-    menuBurger = document.querySelector('.menu__burger');
-    menuBlock = document.querySelector('.menu__inner');
-    menuBox = document.querySelector('.menu__box');
-    
-window.addEventListener('scroll', checkScroll);
-document.addEventListener('DOMContentLoaded', checkScroll);
-enterBtn.addEventListener('click', onEnterBtn);
-btnClose.addEventListener('click', onBtnClose);
-menuBtn.addEventListener('click', onMenuBtn);
-menuBurger.addEventListener('click', onMenuBurger);
-
-function checkScroll() {
-    let scrollPos = window.scrollY;
-
-    if(scrollPos > 0) {
-        header.classList.add('sticky');
-    } else {
-        header.classList.remove('sticky');
-    }
-}
-
-function onEnterBtn() {
-    overlay.classList.add('active')
-    modalEl.classList.add('active')
-    menuBlock.classList.remove('active')
-    menuBox.classList.remove('active')
-    menuBurger.classList.remove('active')
-    bodyLock.classList.add('lock')
-};
-
-function onBtnClose() {
-    overlay.classList.remove('active')
-    modalEl.classList.remove('active')
-    bodyLock.classList.remove('lock')
-};
-
-function onMenuBtn() {
-    menuBtn.classList.toggle('active')
-    menuForm.classList.toggle('active')
-    menuSearch.classList.toggle('active')
-};
-
-function onMenuBurger() {
-    menuBlock.classList.toggle('active')
-    menuBurger.classList.toggle('active')
-    overlay.classList.toggle('_active')
-    overlay.classList.remove('active')
-    menuBox.classList.toggle('active')
-    modalEl.classList.remove('active')
-    bodyLock.classList.add('lock')
-};
-
-menuBurger.addEventListener('click', function(){
-    let menuBurger = document.querySelector('.menu__burger')
-    menuBurger.addEventListener('click', function(){
-        bodyLock.classList.toggle('lock')
-    })
-})
-
 let tab = function () {
-    let tabNav = document.querySelectorAll('.btns-tabs__btn'),
-        tabContent = document.querySelectorAll('.modal-form__item'),
+    let tabNav = document.querySelectorAll('.tabs-btns__btn'),
+        tabContent = document.querySelectorAll('.creations-details__item'),
         tabName;
 
     tabNav.forEach(item => {
@@ -279,10 +306,114 @@ let tab = function () {
             item.classList.contains(tabName) ? item.classList.add('active') : item.classList.remove('active');
         })
     }
-
 };
 
 tab();
+
+
+let header = document.querySelector('.header');
+    enterBtn = document.querySelector('.menu__enter-btn');
+    modalEl = document.querySelector('.modal');
+    overlay = document.querySelector('.overlay');
+    btnClose = document.querySelector('.btn--close');
+    bodyLock = document.getElementById('body');
+    menuBtn = document.querySelector('.menu__btn');
+    menuForm = document.querySelector('.menu__form');
+    menuSearch = document.querySelector('.menu__search');
+    menuBurger = document.querySelector('.menu__burger');
+    menuBlock = document.querySelector('.menu__inner');
+    menuBox = document.querySelector('.menu__box');
+    menuCursour = document.querySelector('.menu__item--dropdown');
+    filtersBtn = document.querySelector('.modern__filters-btn')
+    filters = document.querySelector('.creations__filters')
+    filtersCloseBtn = document.querySelector('.creations__close-btn')
+    // tabsBtns = document.querySelector('.tabs-btns__btn')
+    
+window.addEventListener('scroll', checkScroll);
+document.addEventListener('DOMContentLoaded', checkScroll);
+enterBtn.addEventListener('click', onEnterBtn);
+btnClose.addEventListener('click', onBtnClose);
+menuBtn.addEventListener('click', onMenuBtn);
+menuBurger.addEventListener('click', onMenuBurger);
+menuCursour.addEventListener('click', onMenuCursour);
+filtersBtn.addEventListener('click', onFiltersBtn);
+filtersCloseBtn.addEventListener('click', onFiltersCloseBtn);
+tabsBtns.addEventListener('click', onTabsBtns);
+
+function checkScroll() {
+    let scrollPos = window.scrollY;
+
+    if(scrollPos > 0) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+}
+
+function onMenuCursour() {
+    menuCursour.classList.toggle('active')
+}
+
+function onEnterBtn() {
+    overlay.classList.add('active')
+    modalEl.classList.add('active')
+    menuBlock.classList.remove('active')
+    menuBox.classList.remove('active')
+    menuBurger.classList.remove('active')
+    bodyLock.classList.add('lock')
+    filters.classList.remove('active')
+};
+
+function onMenuBtn() {
+    menuBtn.classList.toggle('active')
+    menuForm.classList.toggle('active')
+    menuSearch.classList.toggle('active')
+};
+
+function onMenuBurger() {
+    menuBlock.classList.toggle('active')
+    menuBurger.classList.toggle('active')
+    overlay.classList.toggle('_active')
+    overlay.classList.remove('active')
+    menuBox.classList.toggle('active')
+    modalEl.classList.remove('active')
+    bodyLock.classList.add('lock')
+    filters.classList.remove('active')
+
+    // if(menuBurger) {
+    //     if(menuBurger === 'active') {
+    //         bodyLock.classList.add('lock')
+    //     } else if(menuBurger.classList.remove('active')) {
+    //         bodyLock.classList.remove('lock')
+    //     }
+    // }
+    
+};
+
+menuBurger.addEventListener('click', function(){
+    let menuBurger = document.querySelector('.menu__burger')
+    menuBurger.addEventListener('click', function(){
+        bodyLock.classList.toggle('lock')
+    })
+})
+
+function onFiltersBtn() {
+    filters.classList.add('active')
+    overlay.classList.add('active')
+    bodyLock.classList.add('lock')
+}
+
+function onBtnClose() {
+    overlay.classList.remove('active')
+    modalEl.classList.remove('active')
+    bodyLock.classList.remove('lock')
+};
+
+function onFiltersCloseBtn() {
+    overlay.classList.remove('active')
+    filters.classList.remove('active')
+    bodyLock.classList.remove('lock')
+};
 
 window.increment = function(event) {
     var btn = event.target;
@@ -290,3 +421,6 @@ window.increment = function(event) {
     window.clicks = (window.clicks || 0) + btn.clicks * 2 - 1;
     document.getElementById("clicks").innerText = window.clicks;
 }
+
+
+
